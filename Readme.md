c9ffb7ff0f3155d1f95d45f678fda219b8acec66
Nico <nicolas.alonso81@gmail.com>
2019-12-12 20:05:24 +0100

    Add modifiers for search and format video codec Move the old movies to backup folder Improve console show information And other issues

--- Readme.md
+++ Readme.md
@@ -31,12 +31,21 @@ Darle permisos de ejecución
 chmod +x /usr/local/bin/recode
 ```
 
+
+
 ## Ejecución
 
-Simplemente muevete a la carpeta donde se localizan los videos y ejecuta
+Ejecuta el comando de la siguiente forma:
 
 ```bash
-recode
+recode carpeta
 ```
 
-Y a disfrutar.
+Donde `carpeta` es la ruta donde se localizan los vídeos a convertir.
+
+Modificadores:
+
+* `-m, --move`: Mueve los vídeos antiguos a una carpeta de copia de seguridad o recuperación. Por defecto los vídeos nuevos y antiguos se almacenan en la misma carpeta.
+* `-b BACKUP, --backup BACKUP`:  Indica el nombre de la carpeta de recuperación, por defecto "`backup`".
+* `-s avi, --search avi`: formato de vídeo a buscar, por defecto: "`avi`"
+* `-f mkv, --format mkv`: formato a convertir, por defecto: "`mkv`"
--- recode.py
+++ recode.py
@@ -10,70 +10,156 @@ class Recode:
 
     # Constructor
     def __init__(self, args):
-        print('[ ] Convertir videos a MKV')
-        # TODO: Parsear parametros de entrada
-        # Establecer folder y parametros para ffmpeg
+        # Inicializar
+        self.total = 0
+        self.omitidos = 0
+        self.posicion = 0
+        self.videos = []
+        self.success = []
+        self.current_folder = os.getcwd()
+        # parse arguments
+        self.arguments()
+        self.header()
+
+    # Mostrar mensaje en pantalla
+    def mensaje(self, tipo, *args):
+        tipo = '[%s]' % tipo
+        print('')
+        if len(args) == 1 and (isinstance(args[0], list) or isinstance(args[0], tuple)):
+            for item in args[0]:
+                print(tipo, item)
+        else:
+            print(tipo, *args)
+        print('')
+
+    # Mostrar encabezado en pantalla
+    def header(self):
+        msgs = ['', 'Recode :: Recodificar videos', '', '          (ffmpeg)', '']
+        self.mensaje(' ', msgs)
 
     # Definir los argumentos por linea de comandos
     def arguments(self):
-        self.parser = ArgumentParser()
+        parser = ArgumentParser()
+        parser.add_argument('carpeta', default=".", help="Carpeta donde escanear videos")
+        parser.add_argument('-m', '--move', action="store_true", help="Mover los antiguos videos a una carpeta de historización")
+        parser.add_argument('-b', '--backup', default='backup', help="Nombre de la carpeta Backup")
+        parser.add_argument('-s', '--search', default='avi', help="Formato de video a buscar (defecto: AVI)")
+        parser.add_argument('-f', '--format', default='mkv', help="Formato a convertir el video (defecto: MKV)")
+        self.args = parser.parse_args()
+        self.get_folder()
 
+    # Obtener la carpeta de busqueda
+    def get_folder(self):
+        folder = self.args.carpeta
+        if not folder.startswith('/'):  # Ruta relativa
+            if self.args.carpeta == '.':
+                folder = self.current_folder
+            else:
+                if self.args.carpeta.startswith('.'):
+                    parts = self.args.carpeta.split('/')
+                    parts.remove('.')
+                    folder = os.path.join(self.current_folder, *parts)
+                else:
+                    folder = os.path.join(self.current_folder, self.args.carpeta)
+        self.args.carpeta = folder
+        return folder
 
     # Recuperar el listado de ficheros
     def get_files(self):
-        print('[+] Buscar videos en formato AVI')
-        print('')
-        self.videos = []
+        msgs = [
+            'Buscar vídeos con formato: ' + self.args.search,
+            'En la carpeta: ' + self.args.carpeta
+        ]
+        self.mensaje('+', msgs)
+
         for r, d, f in os.walk('.'):
-            for file in f:
-                if '.avi' in file:
-                    self.videos.append( file )
-                    print(' > ', file)
+            if r == '.':  # Deshabilitar modo recursivo
+                for file in f:
+                    if file.endswith('.avi'):
+                        self.videos.append( file )
+                        print(' > ', file)
 
         self.total = len(self.videos)
-        print('')
-        print('[+] Total videos', self.total)
+        self.mensaje('+', 'Total videos encontrados: ', self.total)
+
+    # Create folder backup
+    def create_folder_backup(self):
+        self.backup_folder = os.path.join(self.args.carpeta, self.args.backup)
+        if not os.path.isdir(self.backup_folder):
+            self.mensaje('+', ['Create backup folder:', self.backup_folder])
+            os.mkdir( self.backup_folder )
 
     # Transformar
     def convert(self, video):
-        print(' ')
-        print('[+] Codificar video: ', video)
-        video_mkv = video.split('.')[0] + '.mkv'
+        self.mensaje('+', 'Codificar vídeo:', video)
+        parts = video.split('.')
+        parts.pop()
+        parts.append(self.args.format)
+        new_video = '.'.join(parts)
 
         # Verificar si el video en mkv ya existe en el directorio
-        if os.path.isfile(video_mkv):
+        if os.path.isfile(new_video):
             self.omitidos += 1
-            print('[!] El video ya existe, omitir')
+            self.mensaje('!', 'El vídeo ya existe, omitir')
         else:
             # Convertir video
-            self.success.append( video_mkv )
-            p = subprocess.Popen(('ffmpeg', '-i', video, video_mkv))
+            self.success.append( new_video )
+            p = subprocess.Popen(('ffmpeg', '-i', video, new_video))
             p.wait()
+
+            # check ffmpeg return code
+            if p.returncode != 0:
+                self.mensaje('!', 'Error en ffmpeg')
+                sys.exit(1)
+            # Mover el video a la carpeta de backup
+            elif self.args.move:
+                old_path = os.path.join(self.args.carpeta, video)
+                new_path = os.path.join(self.backup_folder, video)
+                self.mensaje('+', ['Mover vídeo a la carpeta backup', new_path])
+
+                if not os.path.isfile(new_path):
+                    os.rename(old_path, new_path)
+                else:
+                    self.mensaje('!', ['Error en backup el fichero ya existe', new_path, 'Se omite'])
 
     # Ejecutar
     def run(self):
+        # Cambiar de carpeta
+        os.chdir(self.args.carpeta)
         self.get_files()
-        self.omitidos = 0
-        self.posicion = 0
-        self.success = []
-        bar = Bar('Progreso', max=self.total)
 
-        for movie in self.videos:
-            self.posicion += 1
-            print('[i] Video %d de %d' % (self.posicion, self.total))
-            self.convert( movie )
+        if self.total == 0:
+            self.mensaje('!', 'Sin videos que codificar')
+            os.chdir(self.current_folder)
+            sys.exit(1)
+        else:
+            # Crear carpeta backup
+            if self.args.move:
+                self.create_folder_backup()
+            for movie in self.videos:
+                self.posicion += 1
+                self.mensaje('-', 'Video %d de %d' % (self.posicion, self.total))
+                self.convert( movie )
+            self.resumen()
 
+        # Volver a la carpeta inicial
+        os.chdir(self.current_folder)
+
+    # Mostrar un resumen al finalizar
+    def resumen(self):
         # Mostrar resumen
-        print('')
-        print('[+] Total videos: ', self.total)
-        print('[+] Total omitidos', self.omitidos)
-        print('[+] Total convertidos', len(self.success))
-        print('')
+        msgs = [
+            'Total videos: ' + self.total,
+            'Total omitidos' + self.omitidos,
+            'Total convertidos' + len(self.success)
+        ]
+        self.mensaje('+', msgs)
+
         print('[-] Listado de videos convertidos:')
         for v in self.success:
             print(' >> ', v)
-        print('')
-        print('[+] Finalizado')
+
+        self.mensaje('+', 'Finalizado')
 
 
 if __name__ == '__main__':
